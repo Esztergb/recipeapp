511a9e4b7b1b48adf47282ba0943f5f8b0b3d262
Eszter Barbuscia <gubiscom@yahoo.com>
2023-09-10 20:24:16 -0700

    fixed merch conflicts and cleared out code a little

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,9 @@
-// import { Route, Routes } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
-// import Home from "./pages/Home";
-// import Calendar from "./pages/Calendar";
-// import Shopping from "./pages/Shopping";
-// import Pages from "./pages/Pages"
-
-
-// function App() {
-//   return (
-//     <>
-//       <Navbar />
-        
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/calendar" element={<Calendar />} />
-//           <Route path="/shopping" element={<Shopping />} />
-//         </Routes>
-
-//      <Pages />
-//       <Footer />
-//     </>
-//   );
-// }
-
-// export default App;
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
 import { BrowserRouter } from "react-router-dom";
 import Search from "./components/Search";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import Calendar from "./pages/Calendar";
-import Shopping from "./pages/Shopping";
-import SignUp from "./pages/SignUp";
-
-function App() {
-  return (
-    <>
-      <Navbar />
-        
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/shopping" element={<Shopping />} />
-          <Route path="/signup" element={<SignUp />} />
-
-        
-        </Routes>
-      <Footer />
-    </>
-import Footer from "./components/Footer"
-
-// import { GiKnifeFork } from "react-icons/gi";
 
 function App() {
   return (
@@ -67,6 +17,7 @@ function App() {
         <Footer />
       </BrowserRouter>
     </div>
+    
   );
 }
 
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -5,16 +5,23 @@ import Cuisine from "./Cuisine";
 import Searched from "./Searched";
 import Recipe from "./Recipe";
 import { AnimatePresence } from "framer-motion";
+import SignUp from "./SignUp";
+import Shopping from "./Shopping";
+import Calendar from "./Calendar";
+
 
 function Pages() {
   const location = useLocation();
   return (
     <AnimatePresence>
       <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<Home />} />
-          <Route path="/cuisine/:type" element={<Cuisine />} />
-          <Route path="/searched/:search" element={<Searched />} />
-          <Route path="/recipe/:name" element={<Recipe />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/cuisine/:type" element={<Cuisine />} />
+        <Route path="/searched/:search" element={<Searched />} />
+        <Route path="/recipe/:name" element={<Recipe />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/shopping" element={<Shopping />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </AnimatePresence>
   );
